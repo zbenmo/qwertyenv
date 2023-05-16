380c122fe0e4c0acf70c2ca5f3b9a4c22a93aa1b
Oren <zbenmo@gmail.com>
2023-05-16 16:10:52 +0200

    take 5

--- docs/collect_coins.md
+++ docs/collect_coins.md
@@ -2,8 +2,8 @@
 
 CollectCoins is a Chess like game. Each of the two players, white and black, has one piece, a knight (one can also have rock vs. rock, or also additional combinations).
 On the board squares there are coins.
-Each player at its turn move its piece to one of the valid places where the piece can move. If the target square had a coin, the coin is consumed by that player. In this game pieces can not eat one another and cannot stand on the same square.
-The aim of the game is to collect more coins than the other player, by planning better the path, blocking the opponent, etc.
+Each player at its turn moves its piece to one of the valid places where the piece can move. If the target square had a coin, the coin is consumed by that player. In this game, pieces can not eat one another and cannot stand on the same square.
+The aim of the game is to collect more coins than the other player, by planning better the path, by blocking the opponent, etc.
 
 ``` title="Shown below the initial board state and then the board after both players took their turn"
 ---------------------------------
@@ -48,11 +48,11 @@ The aim of the game is to collect more coins than the other player, by planning 
 
 In qwertyenv, at the moment, there are two implementations. One is a Gymnasium environment in which the other player takes a random (legal) move, and another is a PettingZoo environment. Both environments use a common module 'collect_coins_game.py'.
 
-When developing and experimenting with this environment, a few relevant wrappers were also developed, such as EnsureValidAction, that overrides your move if it is not legal, UpDownLeftRight that changes the action_space to {Up, Down, Left, Right}.
+While experimenting with this environment, a few relevant wrappers were also developed, such as EnsureValidAction, that overrides your move if it is not legal, UpDownLeftRight that changes the action_space to {Up, Down, Left, Right}.
 
-Also developed, and hopefully useful, are two utility functions to wrap (any) PettingZoo environment into the matching Gymnasium environment by declaring which is the "external agent" and where to fetch the action for the other agents.
+Also developed, and hopefully useful, are two utility functions to wrap (any) PettingZoo environment into the matching Gymnasium environment by declaring which is the "external agent", and where to fetch the action for the other agents.
 
-The action space for both the Gymnasium and for the PettingZoo environments is the target square. This action spaces was choosen as to unify the action space with various pieces. My experience with this action space is that it is slower to learn than if the action space is specific for a piece (ex. {Up, Down, Left, Right}).
+The action space for both the Gymnasium and for the PettingZoo environments is the target square. This action spaces was choosen as to unify the action space with various pieces. My experience with this action space is, that it is slower to learn with it, than if the action space is specific for a piece (ex. {Up, Down, Left, Right}).
 
 The observation space is the board (with the coins presence/absence), and the location of the two pieces.
 
@@ -66,4 +66,4 @@ self.observation_space = gym.spaces.Dict(spaces=obs_space)
 self.action_space = gym.spaces.MultiDiscrete([8, 8])
 ```
 
-To initialize the environment pass the desired pieces (ex. 'rock' and 'rock'). Please note that a 'rock' here is a limited version that can only move one square at a time. You need also to state if you wish to go first or second (player=0 or player=1)
+To initialize the environment pass the desired pieces (ex. 'rock' and 'rock'). Please note that a 'rock' here is a limited version that can only move one square at a time. You are expected also to indicate if you wish to go first or second (player=0 or player=1)
---
+++ qwertyenv/take_5_game.py
@@ -0,0 +1,124 @@
+# This game is called also 6-Nimmt (in German).
+# Base this implementation on https://github.com/johannbrehmer/rl-6-nimmt
+
+from dataclasses import dataclass, field
+import random
+import numpy as np
+from functools import reduce
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+@dataclass
+class Player:
+    cards: list = field(default_factory=list)
+    negative_points : int = 0
+
+    def __str__(self):
+        return '\n'.join([
+            'cards in hand: ' + ', '.join(map(Take5Game._format_card, self.cards)),
+            f'negative points: {self.negative_points:>3d}'
+        ])
+
+
+class Take5Game:
+    """ Take5Game (6-Nimmt) """
+
+    def __init__(self, num_players, num_rows=4, num_cards=104, threshold=6):
+        self._players = [Player() for _ in range(num_players)]
+        self._board = [[] for _ in range(num_rows)]
+        self._threshold = threshold
+
+        self._cards = list(range(1, num_cards + 1))
+        random.shuffle(self._cards)
+
+        for player in self._players:
+            selected_cards, self._cards = self._cards[:10], self._cards[10:] # 10 cards per each player
+            player.cards.extend(sorted(selected_cards))
+
+        # Each row has always at least one card
+        for row in self._board:
+            row.append(self._cards.pop(0))
+
+    def step(self, cards: list[int]):
+        assert len(cards) == len(self._players)
+        for o in np.argsort(cards):
+            player, card = self._players[o], cards[o]
+            player.cards.remove(card) # card must be there
+            # now in which row the card goes?
+            row_idx = None
+            for i, row in enumerate(self._board):
+                assert len(row) > 0
+                if row[-1] > card:
+                    continue
+                if row_idx is None or self._board[row_idx][-1] > row[-1]:
+                    row_idx = i
+            if row_idx is None:
+                # no row fits, user selects a row and takes the cards there (leaving the new card)
+                # TODO: pick a row
+                row_idx = 0 # TODO:
+                self._take_cards(player, row_idx)
+            else:
+                if len(self._board[row_idx]) + 1 >= self._threshold:
+                    self._take_cards(player, row_idx)
+            self._board[row_idx].append(card)
+
+    def is_done(self) -> bool:
+        return len(self._players[0].cards) < 1
+
+    def _take_cards(self, player, row_idx):
+        player.negative_points += reduce(lambda till_now, c: till_now + Take5Game._card_value(c), self._board[row_idx])
+        self._board[row_idx].clear()
+
+    def render(self):
+        """ Report game progress somehow """
+
+        logger.info("-" * 120)
+        logger.info("Board:")
+        for _, cards in enumerate(self._board):
+            logger.info(f"  " + " ".join([self._format_card(card) for card in cards]) + "   _ " * (self._threshold - len(cards) - 1) + "   * ")
+        logger.info("\nPlayers:")
+        for p, player in enumerate(self._players):
+            logger.info(
+                f"{p})\t{player})\n"
+            )
+        # if self._is_done():
+        #     winning_player = np.argmin(self._scores)
+        #     losing_player = np.argmax(self._scores)
+        #     logger.info(f"The game is over! {self._player_name(winning_player)} wins, {self._player_name(losing_player)} loses. Congratulations!")
+        logger.info("-" * 120)
+
+    @staticmethod
+    def _card_value(card):
+        """ Returns the points (Hornochsen) on a single card """
+
+        assert 1 <= card <= 104
+
+        if card == 55:
+            return 7
+        elif card % 11 == 0:  # 11, 22, ..., 99; but not 55
+            return 5
+        elif card % 10 == 0:  # 10, 20, 30, ..., 100
+            return 3
+        elif card % 10 == 5:  # 5, 15, ..., 95, but not 55
+            return 2
+        else:
+            return 1
+
+    @staticmethod
+    def _format_card(card):
+        signs = {1: " ", 2: ".", 3: ":", 5: "+", 7: "#"}
+        value = Take5Game._card_value(card)
+        return f"{card:>3d}{signs[value]}"
+
+
+if __name__ == "__main__":
+    take5 = Take5Game(3)
+    take5.render()
+
+    while not take5.is_done():
+        selected_cards = [p.cards[0] for p in take5._players]
+        take5.step(selected_cards)
+        take5.render()
